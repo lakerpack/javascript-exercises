1a9e24a4946678b56c2c3fc666dbd60d2c999666
lackerpack <thapaanil2007@gmail.com>
2022-07-17 18:55:59 -0500

    Solve prob07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  newtemp = ((temp-32)/1.8);
+  return Math.round(newtemp*10)/10;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  newtemp = ((temp*1.8)+32);
+  return Math.round(newtemp*10)/10;
 };
 
 // Do not edit below this line
