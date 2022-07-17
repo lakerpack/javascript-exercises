01c057c2fcafb5d8d359b7e9091582621a6faab7
lackerpack <thapaanil2007@gmail.com>
2022-07-17 12:45:46 -0500

    Solve prob02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+    let base = "";
+    if (times < 0){
+        return "ERROR";
+    }
+    else{
+        for (let i = 0; i < times; i++){
+            base += string;
+        }
+    }
+    return base;
 };
 
 // Do not edit below this line
