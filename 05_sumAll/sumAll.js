73e500e0a04275848680654b7e43d2781c4ab2a4
lackerpack <thapaanil2007@gmail.com>
2022-07-17 13:50:46 -0500

    Solve prob05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    let sum = 0;
+    if (first < 0 || second < 0 || !Number.isInteger(first) || !Number.isInteger(second)){
+        return "ERROR";
+    }
 
+    if (first >= second){
+        let low = second;
+        let high = first;
+        for (let i = low; i <= high; i++){
+            sum += i;
+        }
+    }
+    else{
+        let low = first;
+        let high = second;
+        for (let i = low; i <= high; i++){
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
