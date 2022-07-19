a5227e64521dffd2c843df68c6b9b232f9f775bd
lackerpack <thapaanil2007@gmail.com>
2022-07-19 17:00:42 -0500

    Finish exercise12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currentYear = 2022;
+    let oldest = people.sort(function(one, two) {
+        if(!("yearOfDeath" in one)){
+            one.yearOfDeath = currentYear;
+        }
 
+        if(!("yearOfDeath" in two)){
+            two.yearOfDeath = currentYear;
+        }
+        const first = one.yearOfDeath - one.yearOfBirth;
+        const second = two.yearOfDeath - two.yearOfBirth;
+        return first > second ? -1 : 1;
+    });
+    console.log(oldest);
+    return oldest[0];
 };
 
 // Do not edit below this line
