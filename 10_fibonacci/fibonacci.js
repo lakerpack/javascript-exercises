d65c742d88aa623ff207cdd597683790a3c89389
lackerpack <thapaanil2007@gmail.com>
2022-07-19 16:23:36 -0500

    Complete exercise10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    let fibo = [1, 1]
+    let realindex = index-1;
+    if(index < 0){
+        return "OOPS";
+    }
+    for (let i = fibo.length-1; i < realindex; i++){
+        fibo.push(fibo[i]+fibo[i-1])
+    }
+    return fibo[realindex];
 };
 
 // Do not edit below this line
